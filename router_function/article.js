c17089c47da0ed2a57d1aed94b3886011968ad94
bigjihua <bigjihua@Gmail.com>
2022-07-07 01:03:58 +0800

    7-6优化接口

--- router_function/article.js
+++ router_function/article.js
@@ -72,32 +72,19 @@ exports.article_put = (req, res) => {
   const sql = `select * from ev_articles where article_id=?`
   db.query(sql, UID, (err, results) => {
     if (err) return res.cc(err)
-    if (results.length === 2) return res.cc('UID重复!WARN! 备份好数据，请重新发布')
+    if (results.length !== 0) return res.cc('UID重复!WARN! 备份好数据，请重新发布')
     put_data.article_id = UID
-    const sql = `SELECT * FROM ev_articles WHERE title=? or article_id=?`
-    db.query(sql, [put_data.title, put_data.article_id], (err, results) => {
+    const sql = `SELECT * FROM ev_articles WHERE title=?`
+    db.query(sql,put_data.title, (err, results) => {
       const err1 = '文章已经被发布过啦！请换一个标题吧!'
-      const err2 = `文章标题和文章id被占用！`
-      const err3 = `!WARN 文章id被占用!`
       if (err) return res.cc(err)
-      if (results.length === 2) return res.cc(err1)
-      if (
-          results.length === 1 &&
-          results[0].title === put_data.title &&
-          results[0].article_id === put_data.article_id
-      )
-        return res.cc(err1)
-      if (results.length === 1 && results[0].title === put_data.title)
-        return res.cc(err2)
-      if (results.length === 1 && results[0].article_id === put_data.article_id)
-        return res.cc(err3)
+      if (results.length !== 0) return res.cc(err1)
       // 插入sql
+      put_data.state = '已发布'
       const sql = `insert into ev_articles set ?`
       db.query(sql, put_data, (err, results) => {
         if (err) return res.cc(err)
-        if (results.affectedRows !== 1)
-          return res.cc('发布文章失败，请重新再试')
-        if (results.length === 0) return res.cc('发布文章失败 !')
+        if (results.affectedRows !== 1) return res.cc('发布文章失败，请重新再试')
         res.status(200).send({
           message: '发布文章成功!',
           article: UID
@@ -170,38 +157,35 @@ exports.article_image = (req,res) => {
 
 // 新增名下图库照片
 exports.article_upimage = (req,res) => {
-  const path = 'http://127.0.0.1/public/uploads/' + req.file.originalname
+  const path = setting.selpath + req.file.originalname
   const username = req.body.username
-  const userimage = path
   if (req.file.length === 0) res.cc('上传文件不能为空')
   const data = {
     username: username,
-    userimage: userimage
+    userimage: path
   }
   const sql = `insert into ev_userimage set ?`
   db.query(sql,data,(err,results)=>{
     if(err) return res.cc(err)
-    if (results.affectedRows !== 1)
-      return res.cc('存储失败，请重新再试')
-    if (results.length === 0) return res.cc('存储路径错误 !请联系管理员')
+    if (results.affectedRows !== 1) return res.cc('存储失败，请重新再试',406)
     res.status(200).send({
       status:200,
       message: '文件接收成功'
     })
   })
-} // 新增图片
+}
 
 // 删除名下图库照片
 exports.article_imagedel = (req,res) => {
   const body = req.body
   const sql = `select * from ev_userimage where username=?`
   db.query(sql,body.picusername,(err,results)=>{
-    if(err) return res.cc(err)
-    if(results.length === 0 ) return res.cc('操作错误')
+    if(err) return res.cc(err,500)
+    if(results.length === 0 ) return res.cc('操作错误',406)
     const sql = `delete from ev_userimage where id=?`
     db.query(sql,body.id,(err,results)=>{
       if(err) return res.cc(err)
-      if (results.affectedRows !== 1) return res.cc('删除失败')
+      if (results.affectedRows !== 1) return res.cc('删除失败',406)
       res.status(200).send({
         status: 200,
         message:'删除成功'
--- setting.js
+++ setting.js
@@ -3,16 +3,18 @@
 const kuo = 80
 // 不进行token验证的子口
 const api = [/^\/data|\/my|\/archives\/|\/public\//]
+// 部署API
 const cagapi = [/^\/api\/data|\/api\/my|\/api\/archives\/|\/api\/public\//]
 // 文件上传路径
 const path = 'C:\\Users\\jihua\\Desktop\\node\\api111\\api\\public/uploads'
+// 文件访问路径
 const selpath = 'https://jihau.top/public/uploads/'
 /*　生成格式化日期 */
 const dayjs = require('dayjs')
 let d = new Date()
 const pub_date = dayjs(d).format('YYYY-MM-DD')
 const pub_month = dayjs(d).format('MM')
-/* 生成随机ＵＩＤ */
+// 生成随机ＵＩＤ
 const art_idnum = Math.floor(Math.random() * (100 - 10 + 1)) + 10
 let str = ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
 function generateMixed(n) {
@@ -23,58 +25,7 @@ function generateMixed(n) {
       }
       return res + art_idnum;
 }
-//
-// // 定时清理ev_userartdata 点赞和评论都为0的函数
-// const db = require('./database/linkdb')
-// function clearUserartdata () {
-//     const promise = new Promise((resolve,reject)=>{
-//         const sql = `select
-//         ev_userartdata.goodnum,ev_userartdata.collect
-//         from
-//         ev_userartdata
-//         where goodnum =0 and collect = 0
-//         `
-//         db.query(sql,(err,results) => {
-//             if(err) {
-//                 const data = {
-//                     err: err,
-//                     log: '自动化处理点赞和评论失败',
-//                     user: 'admin',
-//                     pub_date: pub_date
-//                 }
-//                 db.query(`insert into ev_error_log set ?`,data,(err,results)=>{
-//                     if(err) return
-//                 })
-//             }
-//             const sql = `update ev_userartdata set state=1 where goodnum=0 and collect=0`
-//             db.query(sql,(err,results)=>{
-//                 if(err) {
-//                     const data = {
-//                         err: err,
-//                         log: '自动化处理点赞和评论失败',
-//                         user: 'admin',
-//                         pub_date: pub_date
-//                     }
-//                     db.query(`insert into ev_error_log set ?`,data,(err,results)=>{
-//                         if(err) return
-//                     })
-//                 }
-//                 const data = {
-//                     err: results,
-//                     log: '自动化处理点赞和评论成功',
-//                     user: 'admin',
-//                     pub_date: pub_date
-//                 }
-//                 db.query(`insert into ev_error_log set ?`,data,(err)=>{
-//                     if(err) return
-//                 })
-//             })
-//         })
-//     })
-//     console.log('执行')
-// }
-//
-// setInterval(clearUserartdata,1000*60*60*18)
+
 
 module.exports={
       kuo,
