7c5e28c2a07129bbac29a16a93e59e56e9e2f24d
JiHua <88818118+bigJiHua@users.noreply.github.com>
2022-08-17 16:01:41 +0800

    Update linkdb.js

--- database/linkdb.js
+++ database/linkdb.js
@@ -1,10 +1,7 @@
 const mysql = require('mysql')
 
 const db = mysql.createPool({
-    host: '43.129.172.233',
-    user: 'node_mysql',
-    password: 'admin',
-    database: 'my_db_01'
+   
 })
 
 module.exports = db
