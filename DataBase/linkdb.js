84032f7a90e7025bb3d464488d4b24da89d7dbdf
bigjihua <bigjihua@gmail.com>
2023-10-28 18:02:13 +0800

    Update linkdb.js

--- DataBase/linkdb.js
+++ DataBase/linkdb.js
@@ -1,12 +1,5 @@
 const mysql = require('mysql')
 
-// const db = mysql.createPool({
-//   host: '47.115.225.101',
-//   user: 'my_db_01',
-//   password: 'admin',
-//   database: 'my_db_01'
-// })
-
 const db = mysql.createPool({
   host: '127.0.0.1',
   user: 'root',
